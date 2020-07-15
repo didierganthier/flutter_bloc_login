e1ad15fb345c47d81bb8d5c436f37b76650e794d
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-14 22:39:05 -0400

    login_bloc.dart class created

---
+++ lib/login_bloc.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutterbloclogin/authentification_bloc.dart';
+import 'package:flutterbloclogin/authentification_event.dart';
+import 'package:flutterbloclogin/login_event.dart';
+import 'package:flutterbloclogin/login_state.dart';
+import 'package:flutterbloclogin/user_repository.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(
+      LoginState currentState,
+      LoginEvent event,
+      ) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.dispatch(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
